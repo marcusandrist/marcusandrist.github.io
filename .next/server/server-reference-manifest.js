cf43fda8a020da32310a80023927de4e57d6e95c
Marcus <marcusandrist53@gmail.com>
2024-07-09 18:50:57 -0500

    Added animated background

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Y4i8ElQqip0tS3MJF2QL4B0W4XCENOyLYM5rBpmtgwQ=\"\n}"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,44 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        blob: "blob 7s infinite",
+        blobber: "blobber 1s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+        blobber: {
+          "0%": {
+            transform: "translate(-100px, 200px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
